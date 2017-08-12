dbf20ab1441e558e05668e236ef54cf215e4e178
Diego Suarez <diego-jbs@hotmail.com>
2017-08-12 01:43:26 -0500

    Certificate

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -99,5 +99,6 @@
     <script id="__bs_script__">//<![CDATA[
         document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.18.12'><\/script>".replace("HOST", location.hostname));
     //]]></script>
+    <script type="text/javascript" src="https://cdn.ywxi.net/js/1.js" async></script>
 </body>
 </html>
