7dbd61c60c219ce484e8e718484e35a0a0b75d83
Diego Suarez <diego-jbs@hotmail.com>
2017-08-14 09:42:29 -0500

    Se agreaga google analitics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -101,5 +101,15 @@
         document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.18.12'><\/script>".replace("HOST", location.hostname));
     //]]></script>
     <script type="text/javascript" src="https://cdn.ywxi.net/js/1.js" async></script>
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-104490666-1', 'auto');
+        ga('send', 'pageview');
+
+    </script>
 </body>
 </html>
